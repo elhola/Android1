ee926e7f8767310bfd47a0eea45db3c91f2d20ed
Yaroslav <yarmcfly@gmail.com>
2019-03-19 16:34:48 +0200

    commit2

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -10,4 +10,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void onStart( ) {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onResume( ) {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onPause( ) {
+        super.onPause();
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStop( ) {
+        super.onStop();
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onDestroy( ) {
+        super.onDestroy();
+        setContentView(R.layout.activity_main);
+    }
+
 }
